64e8cc46d28322bf7016381bbdc99f45615f9071
Eric Wang <ewang@mantle.local>
2020-04-22 15:48:43 -0500

    update to 1.5.0

--- attorch/optimizers.py
+++ attorch/optimizers.py
@@ -46,8 +46,8 @@ class AdamR(torch.optim.Optimizer):
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
                 beta1, beta2 = group['betas']
 
-                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
 
                 state['step'] += 1
                 beta2_t = beta2 ** state['step']
@@ -60,10 +60,10 @@ class AdamR(torch.optim.Optimizer):
                         (1 - beta2_t) * (N_sma - 4) / (N_sma_max - 4) * (N_sma - 2)
                         / N_sma * N_sma_max / (N_sma_max - 2)) / (1 - beta1 ** state['step'])
                     denom = exp_avg_sq.sqrt().add_(group['eps'])
-                    p.addcdiv_(-step_size, exp_avg, denom)
+                    p.addcdiv_(exp_avg, denom, value=-step_size)
                 else:
                     step_size = group['lr'] / (1 - beta1 ** state['step'])
-                    p.add_(-step_size, exp_avg)
+                    p.add_(exp_avg, alpha=-step_size)
 
         return loss
 
