a56dd750643bd7f1333e5a50ea4da06f41d1351a
Eric Wang <ewang@mantle.local>
2020-07-28 15:32:36 -0500

    simplify module bases

--- attorch/module.py
+++ attorch/module.py
@@ -10,7 +10,6 @@ class Net(nn.Module):
 
         self._input_shape = self.standard_input_shape(input_shape)
         self._output_shape = self.standard_output_shape(output_shape)
-        self._fix = False
 
     @property
     def input_shape(self):
@@ -31,32 +30,6 @@ class Net(nn.Module):
     @property
     def params(self):
         raise NotImplementedError
-
-    @property
-    def fix(self):
-        return self._fix
-
-    @fix.setter
-    def fix(self, fix):
-        if not isinstance(fix, bool):
-            raise ValueError('Expecting <class \'bool\'> but received {}'.format(type(fix)))
-        fix_str = 'Fixing' if fix else 'Not fixing'
-        logger.info('{} {}'.format(fix_str, self.__class__.__name__))
-        self._fix = fix
-        self.requires_grad_(not fix)
-        self.train(not fix)
-
-    def requires_grad_(self, requires_grad=True):
-        if self.fix:
-            return super().requires_grad_(False)
-        else:
-            return super().requires_grad_(requires_grad)
-
-    def train(self, mode=True):
-        if self.fix:
-            return super().train(False)
-        else:
-            return super().train(mode)
 
 
 class NetParams(Net):
